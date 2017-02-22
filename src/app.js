b66c1a2b5c52766f0f814dd2cc9cf9228ae308d8
Arvid <arvid.bild@studentvikarie.se>
2017-02-22 12:47:33 +0100

    small stuff
    Changed the path of the view file and added "count" to the returned
    objects

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 
 //Set up the view engine
 app.set("view engine", "pug");
-app.set("views", "./public/views");
+app.set("views", "./views");
 
 
 //Gets the passwords etc by using the twit-module
@@ -39,23 +39,23 @@ app.get('/', function (req, res) {
 
     
 //Get the profile name and profile pic from my Twitter account
-Twitter.get("account/verify_credentials", { id_str: "arvid_bild" }, function (err, data, response) {
+Twitter.get("account/verify_credentials", function (err, data, response) {
     
 //create a new objects where we store the data from twitter.     
 var object = new Object();    
 
     
     //get the 5 recent tweets,#retweets,#likes and date of tweet.  
-    Twitter.get("statuses/user_timeline", { id_str: "arvid_bild" }, function (err, data, response) {
+    Twitter.get("statuses/user_timeline", {count: 5}, function (err, data, response) {
         object.tweets = data;
     
 
         //get your 5 most recent friends, profile image, real name, screen-name
-        Twitter.get("friends/list", {screen_name: "ArvidBild"}, function (err,data,response) {    
+        Twitter.get("friends/list", {count: 5}, function (err,data,response) {    
             object.friends = data.users;
             
             //get your 5 most recent private messages, date and time message was sent.
-            Twitter.get("direct_messages", {id_str: "arvid_bild"}, function (err,data,response) {
+            Twitter.get("direct_messages", {count: 5}, function (err,data,response) {
                 object.direct_messages = data;
     
                 //res.render('template.jade', { variableNameToUseInsideJadeTemplate : value });
